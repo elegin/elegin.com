4672d0c8ca729991fd9c164af7316cc0386c26c7
Elegin <elegin@gmail.com>
2022-08-07 14:06:51 -0400

    Update orangejuicepaperfakebook.js

--- assets/js/orangejuicepaperfakebook.js
+++ assets/js/orangejuicepaperfakebook.js
@@ -1,3 +1,3 @@
 var img = new Image();
-img.src = 'https://elegin.com/images/orangejuicepaperfakebook.png';
+img.src = 'https://elegin.com/images/orangejuicepaperfakebook.jpg';
 document.getElementById('mydiv').appendChild(img);
